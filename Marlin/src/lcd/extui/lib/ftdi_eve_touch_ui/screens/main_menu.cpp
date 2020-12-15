354ab648c0237ad5abd178575a3d4c4fd7a9b9d2
Marcio Teixeira <mlt4356-github@yahoo.com>
2020-12-15 10:18:37 -0700

    Only disable steppers which are configured to timeout (#30)

--- Marlin/src/lcd/extui/lib/ftdi_eve_touch_ui/screens/main_menu.cpp
+++ Marlin/src/lcd/extui/lib/ftdi_eve_touch_ui/screens/main_menu.cpp
@@ -96,7 +96,21 @@ bool MainMenu::onTouchEnd(uint8_t tag) {
     case 3: injectCommands_P(PSTR("G12")); GOTO_SCREEN(StatusScreen); break;
     #endif
     case 4:  GOTO_SCREEN(MoveAxisScreen);                             break;
-    case 5:  injectCommands_P(PSTR("M84"));                           break;
+    case 5:  injectCommands_P(PSTR("M18"
+            #if DISABLE_INACTIVE_X
+                " X"
+            #endif
+            #if DISABLE_INACTIVE_Y
+                " Y"
+            #endif
+            #if DISABLE_INACTIVE_Z
+                " Z"
+            #endif
+            #if DISABLE_INACTIVE_E
+                " E"
+            #endif
+        ));
+        break;
     case 6:  GOTO_SCREEN(TemperatureScreen);                          break;
     case 7:  GOTO_SCREEN(ChangeFilamentScreen);                       break;
     case 8:  GOTO_SCREEN(AdvancedSettingsMenu);                       break;
