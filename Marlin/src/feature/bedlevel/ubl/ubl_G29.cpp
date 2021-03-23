af71f97240d03b089219d596f8698ef071ef01d2
Marcio Teixeira <mlt4356-github@yahoo.com>
2021-03-23 11:08:48 -0600

    Fix implementation specific behavior
    - Right bit-shifts on signed quantities is undefined

--- Marlin/src/feature/bedlevel/ubl/ubl_G29.cpp
+++ Marlin/src/feature/bedlevel/ubl/ubl_G29.cpp
@@ -1302,8 +1302,8 @@ mesh_index_pair unified_bed_leveling::find_closest_mesh_point_of_type(const Mesh
 
 #if ENABLED(UBL_HILBERT_CURVE)
 
-  constexpr int8_t  to_fix(int8_t  v) { return v << 1; }
-  constexpr int8_t  to_int(int8_t  v) { return v >> 1; }
+  constexpr int8_t  to_fix(int8_t  v) { return v * 2; }
+  constexpr int8_t  to_int(int8_t  v) { return v / 2; }
   constexpr uint8_t   log2(uint8_t n) { return (n > 1) ? 1 + log2(n >> 1) : 0; }
   constexpr uint8_t  order(uint8_t n) { return uint8_t(log2(n - 1)) + 1; }
 
