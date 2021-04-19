bf5cfd446ff26bcb0eddba539a9e114cb549be05
Marcio Teixeira <mlt4356-github@yahoo.com>
2021-04-19 11:38:06 -0600

    Test 5

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1237,11 +1237,11 @@
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS -49 // <-- changed
-#define Y_MIN_POS -55 // <-- changed
+#define Y_MIN_POS -35 // <-- changed
 #define Z_MIN_POS 0
 #define X_MAX_POS 288 // <-- changed
-#define Y_MAX_POS 284 // <-- changed
-#define Z_MAX_POS 283 // <-- changed
+#define Y_MAX_POS 300 // <-- changed
+#define Z_MAX_POS 294 // <-- changed
 
 /**
  * Software Endstops
@@ -1725,7 +1725,7 @@
 
 #if ENABLED(NOZZLE_PARK_FEATURE)
   // Specify a park position as { X, Y, Z_raise }
-  #define NOZZLE_PARK_POINT {100, 274, 20} // <-- changed
+  #define NOZZLE_PARK_POINT {100, 290, 20} // <-- changed
   //#define NOZZLE_PARK_X_ONLY          // X move only is required to park
   //#define NOZZLE_PARK_Y_ONLY          // Y move only is required to park
   #define NOZZLE_PARK_Z_RAISE_MIN   2   // (mm) Always raise Z by at least this distance
@@ -2755,7 +2755,7 @@
 #define USB_DEVICE_PRODUCT_ID 0x0001 // <-- changed
 #define TOUCH_UI_NO_BOOTSCREEN // <-- changed
 #define TOUCH_UI_ROYAL_THEME // <-- changed
-#define TOUCH_UI_VERSION "Release: 2 (" __DATE__  ")\nMarlin " SHORT_BUILD_VERSION // <-- changed
+#define TOUCH_UI_VERSION "Release: 6 (" __DATE__  ")\nMarlin " SHORT_BUILD_VERSION // <-- changed
 #define TOUCH_UI_FILAMENT_RUNOUT_WORKAROUNDS // <-- changed
 #define CURA_LE_RUNOUT_HANDLING_WORKAROUND // <-- changed
 #define FIL_RUNOUT_PIN 15 // <-- changed
