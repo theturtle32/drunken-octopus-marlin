c5b115cc456a6119c194eb25d7db25bde9db1dad
Marcio Teixeira <mlt4356-github@yahoo.com>
2021-04-19 13:20:15 -0600

    Test 6

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -799,7 +799,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {100, 100, 100, 400} // <-- changed
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {100, 100, 500, 400} // <-- changed
 
 /**
  * Default Max Feed Rate (mm/s)
