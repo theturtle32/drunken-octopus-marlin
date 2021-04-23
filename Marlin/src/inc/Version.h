71fb72496d136e7f9d98e9b51fe1943d35c366c9
Marcio Teixeira <mlt4356-github@yahoo.com>
2021-04-23 11:23:46 -0600

    Test 12

--- Marlin/src/inc/Version.h
+++ Marlin/src/inc/Version.h
@@ -119,5 +119,5 @@
 #else
   #define  USB_DEVICE_PRODUCT_NAME        MACHINE_NAME
 #endif*/
-#define  USB_DEVICE_PRODUCT_NAME          "SynDaver Axi 2"
+#define  USB_DEVICE_PRODUCT_NAME          "SynDaver Axi"
 #define  USB_DEVICE_SERIAL_NAME           "123985739853"
