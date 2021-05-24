8e0d73685ae2c8f6f0a25421434551e6ad84c8f6
Marcio Teixeira <mlt4356-github@yahoo.com>
2021-05-24 11:59:09 -0600

    Improvements to bitmap loading

--- extras-tools/ftdi-eve-lib-examples/png_demo/png_demo.ino
+++ extras-tools/ftdi-eve-lib-examples/png_demo/png_demo.ino
@@ -23,12 +23,16 @@
 
 #include "src/ftdi_eve_lib/ftdi_eve_lib.h"
 #include "png/screen1.h"
-#include "png/screen2.h"
-#include "png/screen3.h"
 
 #include <CRC32.h>
 
 using namespace FTDI;
+
+//#define IS_JPG_OR_PNG
+
+constexpr uint8_t  format   = RGB332;
+constexpr uint16_t bitmap_w = 480;
+constexpr uint16_t bitmap_h = 272;
 
 /***************** SCREEN DEFINITIONS *****************/
 
@@ -58,7 +62,7 @@ class MainScreen : public UIScreen, public UncachedScreen {
       SERIAL_ECHOLNPAIR("Upload CRC: ", crc.finalize());   
     }
  
-    static void draw_background(CommandProcessor &cmd, const void *data, uint16_t len) {
+    static void inflate_background_png_or_jpg(CommandProcessor &cmd, const void *data, uint16_t len) {
       cmd.loadimage(0,OPT_FULLSCREEN)
          .execute();
       send_buffer(cmd, screen1, sizeof(screen1));
@@ -71,15 +75,32 @@ class MainScreen : public UIScreen, public UncachedScreen {
     }
 
     static void inflate_background(CommandProcessor &cmd, const void *data, uint16_t len) {
+      constexpr float scale_w = float(FTDI::display_width)/bitmap_w;
+      constexpr float scale_h = float(FTDI::display_height)/bitmap_h;
+      uint16_t linestride;
+      uint32_t color;
+      switch(format) {
+        case RGB565: linestride = bitmap_w * 2; color = 0xFFFFFFFF; break;
+        case RGB332: linestride = bitmap_w    ; color = 0xFFFFFFFF; break;
+        case L1:     linestride = bitmap_w/8  ; color = 0x00000000; break;
+        case L2:     linestride = bitmap_w/4  ; color = 0x00000000; break;
+        case L4:     linestride = bitmap_w/2  ; color = 0x00000000; break;
+        case L8:     linestride = bitmap_w    ; color = 0x00000000; break;
+      }
       cmd.inflate(0)
          .execute();
       send_buffer(cmd, data, len);
       cmd.wait();
-      cmd.cmd(BITMAP_SOURCE(0))
-         .bitmap_layout(RGB565, 480*2, 272)
-         .bitmap_size(BILINEAR, BORDER, BORDER, 480, 272)
+      cmd.cmd(COLOR_RGB(color))
+         .cmd(BITMAP_SOURCE(0))
+         .bitmap_layout(format, linestride, bitmap_h)
+         .bitmap_size(BILINEAR, BORDER, BORDER, bitmap_w*scale_w, bitmap_h*scale_h)
+         .cmd(BITMAP_TRANSFORM_A(uint32_t(float(256)/scale_w)))
+         .cmd(BITMAP_TRANSFORM_E(uint32_t(float(256)/scale_h)))
          .cmd(BEGIN(BITMAPS))
          .cmd(VERTEX2II(0, 0, 0, 0))
+         .cmd(BITMAP_TRANSFORM_A(256))
+         .cmd(BITMAP_TRANSFORM_E(256))
          .execute();
     }
  
@@ -87,12 +108,12 @@ class MainScreen : public UIScreen, public UncachedScreen {
       CommandProcessor cmd;
       cmd.cmd(CLEAR_COLOR_RGB(0xFFFFFF))
          .cmd(CLEAR(true,true,true))
-         .cmd(COLOR_RGB(0x000000))
-         .font(30)
-         .text(0, 0, display_width, display_height, F("The quick brown fox jumps over the lazy fox"))
          .cmd(COLOR_RGB(0xFFFFFF));
-      //draw_background(cmd, screen1, sizeof(screen1));
-      inflate_background(cmd, screen3, sizeof(screen3));
+      #ifdef IS_JPG_OR_PNG
+        inflate_background_png_or_jpg(cmd, screen1, sizeof(screen1));
+      #else
+        inflate_background(cmd, screen1, sizeof(screen1));
+      #endif
     }
 };
 
