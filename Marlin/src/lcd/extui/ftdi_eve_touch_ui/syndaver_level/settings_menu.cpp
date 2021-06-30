a3f6d28bab01faa2b4a285ff7cdb24cc8a3369e2
Marcio Teixeira <mlt4356-github@yahoo.com>
2021-06-30 14:53:29 -0600

    Fix compilation error.

--- Marlin/src/lcd/extui/ftdi_eve_touch_ui/syndaver_level/settings_menu.cpp
+++ Marlin/src/lcd/extui/ftdi_eve_touch_ui/syndaver_level/settings_menu.cpp
@@ -42,7 +42,7 @@ void SettingsMenu::onRedraw(draw_mode_t what) {
   ui.draw_bkgnd( );
   ui.draw_title( POLY(status_text), F("Settings Menu"));
   ui.draw_tile(  POLY(icon_1), 1,   GET_TEXT_F(MSG_INFO_MENU));
-  ui.draw_tile(  POLY(icon_2), 2,   GET_TEXT_F(MSG_ZPROBE_ZOFFSET), HAS_BED_PROBE);
+  ui.draw_tile(  POLY(icon_2), 2,   GET_TEXT_F(MSG_ZPROBE_ZOFFSET), ENABLED(HAS_BED_PROBE));
   ui.draw_tile(  POLY(icon_3), 3,   GET_TEXT_F(MSG_INTERFACE));
   ui.draw_tile(  POLY(icon_4), 4,   F("Safety"));
   ui.draw_tile(  POLY(icon_5), 5,   F("Advanced"));
